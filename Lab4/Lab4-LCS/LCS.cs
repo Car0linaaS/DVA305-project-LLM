71d76324b5a9147978d6da2ec7764c055a46b6b7
stinaangelica <shk22003@student.mdu.se>
2025-02-21 18:01:55 +0100

    9-GPT-3-1
    Number of corrected errors: 8
    Rows where errors have been corrected: 14, 16, 23, 24, 26, 27, 36, 41

--- Lab4/Lab4-LCS/LCS.cs
+++ Lab4/Lab4-LCS/LCS.cs
@@ -9,22 +9,23 @@ namespace Lab4_LCS
     public class LCS
     {
         // START OF TEST AREA
+        // GPT
         public int GetLcsLength(string string1, string string2)
         {
-            shorterString = string2;
+            string shorterString = string2;
             string longerString = string1;
-            if (string1.Length < string2(Length))
+            if (string1.Length < string2.Length) // Fixed incorrect syntax
             {
                 shorterString = string1;
                 longerString = string2;
             }
             int shorterLength = shorterString.Length;
             int longerLength = longerString.Length;
-            int[] tableArray = int[shorterLength + 1];
-            for (i = 1; i <= longerLength; i++)
+            int[] tableArray = new int[shorterLength + 1]; // Fixed incorrect array initialization
+            for (int i = 1; i <= longerLength; i++) // Fixed missing type for 'i'
             {
-                int prevRowAndColumnValue = 1;
-                for (int j = 1; j < shorterLength; j++)
+                int prevRowAndColumnValue = 0; // Changed initial value from 1 to 0 (LCS starts from 0)
+                for (int j = 1; j <= shorterLength; j++) // Fixed loop boundary condition
                 {
                     int currentValueBeforeOverwritten = tableArray[j];
                     if (longerString[i - 1] == shorterString[j - 1])
@@ -33,13 +34,14 @@ namespace Lab4_LCS
                     }
                     else
                     {
-                        tableArray[j] = Math.Min(tableArray[j - 1], tableArray[j]);
+                        tableArray[j] = Math.Max(tableArray[j - 1], tableArray[j]); // Fixed incorrect function (Max instead of Min)
                     }
                     prevRowAndColumnValue = currentValueBeforeOverwritten;
                 }
             }
-            return tableArray[tableArray.Length -- 1];
+            return tableArray[shorterLength]; // Fixed incorrect index decrement
         }
+
         // END OF TEST AREA
     }
 }
