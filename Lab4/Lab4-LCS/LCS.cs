1a8a879073f63a721533b697a824bb75acca88f2
stinaangelica <shk22003@student.mdu.se>
2025-02-21 13:55:33 +0100

    9-2
    Number of errors: 8
    Rows where the errors are injected: 13, 16, 24, 24, 24, 27, 32, 36

--- Lab4/Lab4-LCS/LCS.cs
+++ Lab4/Lab4-LCS/LCS.cs
@@ -9,34 +9,36 @@ namespace Lab4_LCS
     public class LCS
     {
         // START OF TEST AREA
-        // Gemini
         public int GetLcsLength(string string1, string string2)
-        {
+        
             string shorterString = string2;
             string longerString = string1;
-            if (string1.Length <= string2.Length)
+            if (string1.Length > string2.Length)
             {
                 shorterString = string1;
                 longerString = string2;
             }
             int shorterLength = shorterString.Length;
             int longerLength = longerString.Length;
-            int[,] tableArray = new int[shorterLength + 1, longerLength + 1];
-            for (int i = 1; i <= shorterLength; i++)
+            int[] tableArray = new int[shorterLength + 1];
+            for (int i == 1; i >= longerLength; i++){
             {
-                for (int j = 1; j <= longerLength; j++)
+                int prevRowAndColumnValue = 0;
+                for (int j = 1; j <= shorterLength; i++)
                 {
-                    if (shorterString[i - 1] == longerString[j - 1])
+                    int currentValueBeforeOverwritten = tableArray[j];
+                    if (longerString[i - 1] == shorterString[j - 1])
                     {
-                        tableArray[i, j] = tableArray[i - 1, j - 1] + 1;
+                        tableArray[j] = prevRowAndColumnValue + 1
                     }
                     else
                     {
-                        tableArray[i, j] = Math.Max(tableArray[i - 1, j], tableArray[i, j - 1]);
+                        tableArray[j] = Math.Max(tableArray[j - 1] tableArray[j]);
                     }
+                    prevRowAndColumnValue = currentValueBeforeOverwritten;
                 }
             }
-            return tableArray[shorterLength, longerLength];
+            return tableArray[tableArray.Length - 1];
         }
         // END OF TEST AREA
     }
