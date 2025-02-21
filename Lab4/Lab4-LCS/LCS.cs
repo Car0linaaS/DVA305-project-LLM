01cc1ecb983db7ceef15c4a392a8a42cb143da20
stinaangelica <shk22003@student.mdu.se>
2025-02-21 12:01:42 +0100

    9-GPT-1-2
    Number of corrected errors: 8
    Rows where errors have been corrected: 17, 22, 25, 25, 25, 28, 30, 38

--- Lab4/Lab4-LCS/LCS.cs
+++ Lab4/Lab4-LCS/LCS.cs
@@ -9,7 +9,7 @@ namespace Lab4_LCS
     public class LCS
     {
         // START OF TEST AREA
-        // Gemini
+        // GPT
         public int GetLcsLength(string string1, string string2)
         {
             string shorterString = string2;
@@ -19,24 +19,28 @@ namespace Lab4_LCS
                 shorterString = string1;
                 longerString = string2;
             }
-            int shorterLength = shorterString.Length;
+            int shorterLength = shorterString.Length; // Changed float to int
             int longerLength = longerString.Length;
-            int[,] tableArray = new int[shorterLength + 1, longerLength + 1];
-            for (int i = 1; i <= shorterLength; i++)
+            int[] tableArray = new int[shorterLength + 1]; // Fixed array initialization
+
+            for (int i = 1; i <= longerLength; i++) // Fixed missing semicolon in loop
             {
-                for (int j = 1; j <= longerLength; j++)
+                int prevRowAndColumnValue = 0;
+                for (int j = 1; j <= shorterLength; j++) // Changed `<` to `<=` for correct indexing
                 {
-                    if (shorterString[i - 1] == longerString[j - 1])
+                    int currentValueBeforeOverwritten = tableArray[j]; // Fixed incorrect indexing from `i` to `j`
+                    if (longerString[i - 1] == shorterString[j - 1])
                     {
-                        tableArray[i, j] = tableArray[i - 1, j - 1] + 1;
+                        tableArray[j] = prevRowAndColumnValue + 1;
                     }
                     else
                     {
-                        tableArray[i, j] = Math.Max(tableArray[i - 1, j], tableArray[i, j - 1]);
+                        tableArray[j] = Math.Max(tableArray[j - 1], tableArray[j]);
                     }
+                    prevRowAndColumnValue = currentValueBeforeOverwritten; // Moved outside of else block
                 }
             }
-            return tableArray[shorterLength, longerLength];
+            return tableArray[shorterLength]; // Fixed index to return correct LCS length
         }
         // END OF TEST AREA
     }
