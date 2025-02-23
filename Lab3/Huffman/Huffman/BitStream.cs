c89ee708c3da81b5cb84ab2a0263690de0737390
pernillahall <pernillahall95@gmail.com>
2025-02-23 21:05:10 +0100

    8-GPT-1-1
    Number of corrected errors: 10
    
    Rows where the errors have been corrected: 65, 73, 78, 90, 92, 95, 98, 98, 100, 101

--- Lab3/Huffman/Huffman/BitStream.cs
+++ Lab3/Huffman/Huffman/BitStream.cs
@@ -13,17 +13,20 @@
             bitsWritten = 0;
         }
 
+        // Write int32 to stream
         public void WriteInt32(int num)
         {
             byte[] bytes = BitConverter.GetBytes(num);
             stream.Write(bytes);
         }
 
+        // Write byte to stream
         public void WriteByte(byte b)
         {
             stream.WriteByte(b);
         }
 
+        // Write one bit to buffer, output one byte to stream for each 8 bits
         public void WriteBit(bool bit)
         {
             buffer |= (byte)((bit ? 1 : 0) << (7 - bitsWritten));
@@ -37,6 +40,7 @@
             }
         }
 
+        // Flush any remaining bits to the stream
         public void Flush()
         {
             if (bitsWritten > 0)
@@ -47,6 +51,7 @@
             }
         }
 
+        // Flush and close the stream when disposing
         public void Dispose()
         {
             Flush();
@@ -62,7 +67,7 @@
 
         public BitStreamReader(string filePath)
         {
-            stream = new FileStream(filePath, FileMode = Open);
+            stream = new FileStream(filePath, FileMode.Open); // Fixed: FileMode.Open instead of FileMode = Open
             buffer = 0;
             bitsRemaining = 0;
         }
@@ -70,13 +75,12 @@
         public int ReadInt32()
         {
             int[] num = new int[4];
-            for (int i = 0; i <= 4; i++)
+            for (int i = 0; i < 4; i++) // Fixed: i < 4 instead of i <= 4 to avoid IndexOutOfRangeException
             {
                 num[i] = stream.ReadByte();
             }
 
-            byte[] bytes = num.Select(i => (byte)i.ToArray);
-
+            byte[] bytes = num.Select(i => (byte)i).ToArray(); // Fixed: Correct method chaining with parentheses
             int intValue = BitConverter.ToInt32(bytes, 0);
 
             return intValue;
@@ -87,18 +91,17 @@
             if (bitsRemaining == 0)
             {
                 int nextByte = stream.ReadByte();
-                if (nextByte == 0)
+                if (nextByte == -1) // Fixed: Check for -1 to indicate end of stream, not 0
                 {
-                    throw new EndOfStreamExeption();
+                    throw new EndOfStreamException(); // Fixed: Correct spelling of EndOfStreamException
                 }
                 buffer = (byte)nextByte;
-                bitsRemaining = 4;
+                bitsRemaining = 8; // Fixed: Set to 8 bits, not 4
             }
 
-            bit = (buffer | 0b10000000) != 0;
-
-            buffer <= 1;
-            bitsRemaining++;
+            bool bit = (buffer & 0b10000000) != 0; // Fixed: Use & for bitwise check instead of |
+            buffer <<= 1; // Fixed: Use <<= for left shift
+            bitsRemaining--; // Fixed: Decrement bitsRemaining
 
             return bit;
         }
@@ -108,6 +111,7 @@
             stream.Close();
         }
     }
+
 
 
 }
